0b489d2358e4759c6831547848c1f565111f11c6
bvolvy <bazilevolvy@gmail.com>
2025-06-06 22:25:37 -0400

    Updated SignIn.tsx

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -181,7 +181,7 @@ const SignIn: React.FC = () => {
                 <img 
                   src="/cash.png" 
                   alt="T-Kanè Logo" 
-                  className="w-20 h-20 object-contain"
+                  className="w-50 h-50 object-contain"
                 />
                 {/* Floating sparkles */}
                 <Sparkles className="w-4 h-4 text-purple-200 absolute -top-2 -right-2 animate-pulse" />
